58d77b7f9d490cf6225025659d1a383ce46da8fd
Hugo Ratts <118541950+vratts@users.noreply.github.com>
2024-10-08 14:42:02 -0300

    Update code for Typescript #4

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import { SportradarAPI } from "./src/libs/ts/SportRadar/SportradarAPI.ts";
-
+/**
+ * Importação dinamica para projetos com TypeScript
+ * Os mesmos modulos das classes de entrada são declarados aqui.
+ */
 export {
     SportradarAPI
 }
